1034466b620bb9c38c541f4d31eb781d2aefaeb1
JorCodeSprout <jpradillo7@gmail.com>
2025-10-13 10:11:55 +0200

    Añadido logo

--- src/Controller/HistoricoController.php
+++ src/Controller/HistoricoController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/historico')]
 #[IsGranted('ROLE_ADMIN')]
 class HistoricoController extends AbstractController {
-    #[Route('/historico', 'app_historico', methods:["get"])]
+    #[Route('/', 'app_historico', methods:["get"])]
     public function index(HistoricoRepository $historicoRepository): Response {
         $historicos = $historicoRepository->findAll();
 
